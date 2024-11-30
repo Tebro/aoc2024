0ed00fe41076c449de7152629c9e6436a98847f6
Richard Weber <git@tebro.simplelogin.com>
2024-11-30 13:30:58 +0200

    Slow down test

--- day0/day0.ts
+++ day0/day0.ts
@@ -5,9 +5,9 @@ const step1 = (input: string[]): number => {
 };
 
 const step2 = (input: string[]): number =>
-  input.reduce((acc, line) => {
-    return acc + line.length;
-  }, 0);
+  input.map((line) => line.length)
+    .map((length) => length * 2)
+    .reduce((acc, curr) => acc + curr, 0);
 
 dayHelper({
   day: 0,
