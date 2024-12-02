f476abd652b4eca3e218ab8e2d7bd49aebef4d32
Richard Weber <git@tebro.simplelogin.com>
2024-12-02 08:43:28 +0200

    Fix deps and formatting

--- day1/day1.ts
+++ day1/day1.ts
@@ -1,13 +1,12 @@
-
 import { dayHelper } from "../lib.ts";
-import * as R from 'npm:remeda';
+import * as R from "remeda";
 
 const step1 = (input: string[]): number => {
   const left: number[] = [];
   const right: number[] = [];
 
   for (let i = 0; i < input.length; i++) {
-    const [l, r] = input[i].split('   ');
+    const [l, r] = input[i].split("   ");
     left.push(parseInt(l));
     right.push(parseInt(r));
   }
@@ -17,21 +16,24 @@ const step1 = (input: string[]): number => {
 
   const pairs = R.zip(left, right);
 
-  return pairs.map(([l, r]) => Math.abs(l-r)).reduce((acc, curr) => acc + curr, 0);
-}
+  return pairs.map(([l, r]) => Math.abs(l - r)).reduce(
+    (acc, curr) => acc + curr,
+    0,
+  );
+};
 
 const step2 = (input: string[]): number => {
   const left: string[] = [];
   const right: string[] = [];
 
   for (let i = 0; i < input.length; i++) {
-    const [l, r] = input[i].split('   ');
+    const [l, r] = input[i].split("   ");
     left.push(l);
     right.push(r);
   }
 
-  const x = left.filter(l => right.includes(l))
-    .map(l => {
+  const x = left.filter((l) => right.includes(l))
+    .map((l) => {
       let numMatches = 0;
       for (let i = 0; i < right.length; i++) {
         if (right[i] === l) {
@@ -41,11 +43,11 @@ const step2 = (input: string[]): number => {
       return [parseInt(l), numMatches];
     });
 
-  return x.reduce((acc, [k, v]) => acc + (k*v), 0);
-}
+  return x.reduce((acc, [k, v]) => acc + (k * v), 0);
+};
 
 dayHelper({
   day: 1,
   step1,
-  step2
-})
+  step2,
+});
